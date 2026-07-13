5f8405acb8981bd5323441880c3adb19b97faecb
abeyjaanaah-cpu <abeyjaanaah@gmail.com>
2026-07-14 02:02:09 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -5,7 +5,6 @@ from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
-
 async def is_subscribed(filter, client, update):
     user_id = update.from_user.id
     if user_id in ADMINS:
@@ -34,7 +33,6 @@ async def is_subscribed(filter, client, update):
             pass
             
     return True
-        
 
 async def encode(string):
     string_bytes = string.encode("ascii")
@@ -42,14 +40,12 @@ async def encode(string):
     base64_string = (base64_bytes.decode("ascii")).strip("=")
     return base64_string
 
-
 async def decode(base64_string):
     base64_string = base64_string.strip("=")
     base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
     string_bytes = base64.urlsafe_b64decode(base64_bytes) 
     string = string_bytes.decode("ascii")
     return string
-
 
 async def get_messages(client, message_ids):
     messages = []
@@ -72,7 +68,6 @@ async def get_messages(client, message_ids):
         total_messages += len(temb_ids)
         messages.extend(msgs)
     return messages
-
 
 async def get_message_id(client, message):
     if message.forward_from_chat:
@@ -98,7 +93,6 @@ async def get_message_id(client, message):
     else:
         return 0
 
-
 def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
@@ -119,6 +113,5 @@ def get_readable_time(seconds: int) -> str:
     time_list.reverse()
     up_time += ":".join(time_list)
     return up_time
-
 
 subscribed = filters.create(is_subscribed)
